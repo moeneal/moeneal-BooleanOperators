5ebf264a395e7611942e540f662756611986b6a7
moeneal <momo.mneal@gmail.com>
2023-02-03 04:08:30 +0000

    completed boolean operators lab 2/2/23

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -14,7 +14,8 @@ public class Lab {
      * @return if b is true, return true. if b is false, return false.
      */
     public boolean returnBoolean(boolean b){
-        return false;
+
+        return b;
     }
 
     /**
@@ -23,7 +24,7 @@ public class Lab {
      * look into boolean operations to do this without using if/else statements.
      */
     public boolean returnNot(boolean b){
-        return false;
+        return !b;
     }
 
     /**
@@ -33,7 +34,9 @@ public class Lab {
      * look into boolean operations to do this without using if/else statements.
      */
     public boolean returnAnd(boolean b1, boolean b2){
-        return false;
+
+        boolean b1Equalsb2 = (b1 &&b2) == true;
+        return b1Equalsb2;
     }
 
     /**
@@ -43,7 +46,10 @@ public class Lab {
      * look into boolean operations to do this without using if/else statements.
      */
     public boolean returnOr(boolean b1, boolean b2){
-        return false;
+        
+        boolean result = (b1||b2) == true;
+        return result;
+
     }
 
 
